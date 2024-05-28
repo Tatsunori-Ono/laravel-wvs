9e1f3020f3447c7a42d412190c83c6c066bc2743
Tatsunori-Ono <tatsunori.no1@gmail.com>
2024-05-28 20:22:14 +0100

    installed react and sass.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 // resourcesのCSSとJSをコンパイルしてpublicに移す
 export default defineConfig({
@@ -10,9 +11,12 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/style.css',
-                'resources/js/navbar.js'
+                'resources/js/navbar.js',
+                'resources/sass/app.scss',
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
+        react(),
     ],
 });
