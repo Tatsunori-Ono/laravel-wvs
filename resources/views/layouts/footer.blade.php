4925ba8f5201417d558abd77031553b1d55ddb5d
Tatsunori-Ono <tatsunori.no1@gmail.com>
2024-06-17 13:43:09 +0100

    added the form to external-contact page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer style="background-color: #f8f9fa; padding: 20px; text-align: center;">
+<footer style="background-color: #f8f9fa; padding: 15px; text-align: center;">
     <div>
         <a href="https://www.instagram.com/warwick_vocaloid/" target="_blank">Instagram</a> |
         <a href="https://discord.gg/NXYVdzZpr5" target="_blank">Discord</a> |
@@ -6,10 +6,10 @@
         <a href="https://www.youtube.com/@WarwickVocaloid" target="_blank">YouTube</a> |
         <a href="https://www.warwicksu.com/societies-sports/societies/72239/" target="_blank">{{__('footer.su')}}</a>
     </div>
-    <div style="margin-top: 5px;">
+    <div style="margin-top: 4px;">
         <a href="{{ url('/privacy-policy') }}">{{ __('footer.privacy-policy') }}</a>        
     </div>
-    <div style="margin-top: 5px;">
+    <div style="margin-top: 4px;">
         &copy; Warwick Vocaloid Society 2023
     </div>
 </footer>
